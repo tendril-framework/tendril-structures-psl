b6445674d31d025a310fecf21457b8a96c827438
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2021-03-22 17:21:18 +0530

    Restructure PSL Parsing Infrastructure to prepare for alternate formats

--- src/tendril/structures/parsers/psl.py
+++ src/tendril/structures/parsers/psl.py
@@ -18,79 +18,57 @@ class MetadataParseException(Exception):
     pass
 
 
-class PslParserCSV(ValidatableBase):
-    _delimiter = ','
-
-    _meta = ["CCODE", "VERSION", "COACHNAME"]
-    _owner_ident_name = ["CCODE", "VERSION"]
-    _owner_desc_name = ["COACHNAME"]
-
-    _expected_columns = ["Page", "PsNo", "Lv", "DrawingNo", "Alt", "Item",
-                         "SDrawingNo", "SAlt", "Description", "St", "QPC"]
-
-    _ident_name = [("SDrawingNo", "SAlt"), ("Description", )]
-    _parent_ident_name = [("DrawingNo", "Alt")]
-
-    _qty_name = "QPC"
-    _refdes_name = "Item"
-    _desc_name = "Description"
-
-    _type_name = "St"
-    _type_assembly = "AS"
-    _type_part = "CO"
-
-    _handle_qty = False
+class PslParserBase(ValidatableBase):
+    _meta = ["ident", "revision", "description"]
+    _owner_ident_name = ["{0}r{1}", "ident", "revision"]
+    _owner_desc_name = ["{}", "description"]
 
     def __init__(self, psl_path, vctx=None):
         self._psl_path = psl_path
-        self._meta_data = {}
-        self._columns = []
-        self._active_parents = {}
-        super(PslParserCSV, self).__init__(vctx)
-        self._column_policy = ColumnsRequiredPolicy(self._validation_context,
-                                                    self._expected_columns)
-        self._columns_ok = None
+
+        self._owner = None
+        self.owner_ident = None
+        self.owner_desc = None
+
+        super(PslParserBase, self).__init__(vctx)
 
     @property
     def psl_path(self):
         return self._psl_path
-
-    def _get_psl_file(self):
-        return open(self._psl_path, 'r')
-
-    def _read_meta_line(self, meta_reader, title):
-        rtitle, rvalue = next(meta_reader)
-        try:
-            assert rtitle == title
-        except AssertionError:
-            raise MetadataParseException("Found {0}, Expected {1}".format(rtitle, title))
-        return rvalue
-
-    def _read_meta(self, psl):
-        meta_reader = csv.reader(psl, skipinitialspace=True, quotechar='"', delimiter=",")
-        for meta in self._meta:
-            self._meta_data[meta] = self._read_meta_line(meta_reader, title=meta)
-        self.owner_ident = 'v'.join([self._meta_data[x] for x in self._owner_ident_name])
-        self.owner_desc = ''.join([self._meta_data[x] for x in self._owner_desc_name])
-
-    def _check_columns(self, reader):
-        self._columns = next(reader)
-        try:
-            self._column_policy.check(self._columns)
-            self._columns_ok = True
-        except RequiredColumnMissingError as e:
-            self._validation_errors.add(e)
-            self._columns_ok = False
 
     def _create_owner(self):
         self._owner = GenericEntity()
         self._owner.define(ident=self.owner_ident, desc=self.owner_desc, refdes=None)
         self._owner.structure = BasicContainer(owner=self._owner)
 
+    def _process_meta(self):
+        self.owner_ident = self._owner_ident_name[0].format(*[self._meta_data[x] for x in self._owner_ident_name[1:]])
+        self.owner_desc = self._owner_desc_name[1].format(*[self._meta_data[x] for x in self._owner_desc_name[1:]])
+
+    def _read_meta(self, psl):
+        raise NotImplementedError
+
+    def parse(self):
+        raise NotImplementedError
+
+    def cleanup(self):
+        raise NotImplementedError
+
+
+class PslParserLineReader(PslParserBase):
+    _ident_name = [("{0}", "ident"), ("{}", "description", )]
+    _parent_ident_name = [("{0}", "parent")]
+
+    _handle_qty = True
+
+    def __init__(self, psl_path, vctx=None):
+        self._active_parents = {}
+        super(PslParserLineReader, self).__init__(psl_path=psl_path, vctx=vctx)
+
     @staticmethod
     def _extract_composite_value(line, options):
         for option in options:
-            candidate = ''.join([line[x] for x in option])
+            candidate = option[0].format(*[line[x] for x in option[1:]]).strip()
             if candidate:
                 return candidate
 
@@ -135,12 +113,10 @@ class PslParserCSV(ValidatableBase):
                     parent.structure = BasicContainer(owner=parent)
                     parent.insert(entity)
 
-    def _parse_line(self, line):
-        line = dict(zip(self._columns, line))
-
+    def _process_line(self, line):
         parent_ident = self._extract_parent_ident(line)
 
-        level = int(line['Lv'])
+        level = int(line[self._level_name])
         parents = self._active_parents[level - 1]
         if level > 0:
             assert parents[0].ident == parent_ident
@@ -150,17 +126,18 @@ class PslParserCSV(ValidatableBase):
 
         self._active_parents[level] = line_entities
 
+    def _get_psl_file(self):
+        raise NotImplementedError
+
+    def _read_meta(self, psl):
+        raise NotImplementedError
+
     def parse(self):
         psl = self._get_psl_file()
         self._read_meta(psl)
+        self._process_meta()
 
-        reader = csv.reader(psl, skipinitialspace=True, quotechar='"', delimiter=",")
-        self._check_columns(reader)
-
-        if not self._columns_ok:
-            logger.warn("Expected Columns Not Found in Provided PSL. "
-                        "Not Parsing : " + self._psl_path)
-            return
+        reader = self._prep_reader(psl)
 
         # TODO Consider checking if it exists in the library first,
         #  and if it does, link to the library part
@@ -174,9 +151,102 @@ class PslParserCSV(ValidatableBase):
         return self._owner
 
     def cleanup(self):
+        raise NotImplementedError
+
+
+class PslParserCSV(PslParserLineReader):
+    _delimiter = ','
+
+    _ident_name = [("{0} {1}", "SDrawingNo", "SAlt"), ("{}", "Description",)]
+    _parent_ident_name = [("{0} {1}", "DrawingNo", "Alt")]
+
+    _expected_columns = ["Page", "PsNo", "Lv", "DrawingNo", "Alt", "Item",
+                         "SDrawingNo", "SAlt", "Description", "St", "QPC"]
+
+    _level_name = "level"
+    _qty_name = "qty"
+    _refdes_name = "refdes"
+    _desc_name = "description"
+
+    _type_name = "type"
+    _type_assembly = "Assembly"
+    _type_part = "Part"
+
+    _handle_qty = False
+
+    def __init__(self, psl_path, vctx=None):
+        self._meta_data = {}
+        self._columns = []
+        super(PslParserCSV, self).__init__(psl_path, vctx)
+        self._column_policy = ColumnsRequiredPolicy(self._validation_context,
+                                                    self._expected_columns)
+        self._columns_ok = None
+
+    def _get_psl_file(self):
+        return open(self._psl_path, 'r')
+
+    def _read_meta_line(self, meta_reader, title):
+        rtitle, rvalue = next(meta_reader)
+        try:
+            assert rtitle == title
+        except AssertionError:
+            raise MetadataParseException("Found {0}, Expected {1}".format(rtitle, title))
+        return rvalue
+
+    def _read_meta(self, psl):
+        meta_reader = csv.reader(psl, skipinitialspace=True, quotechar='"', delimiter=",")
+        for meta in self._meta:
+            self._meta_data[meta] = self._read_meta_line(meta_reader, title=meta)
+
+    def _check_columns(self, reader):
+        self._columns = next(reader)
+        try:
+            self._column_policy.check(self._columns)
+            self._columns_ok = True
+        except RequiredColumnMissingError as e:
+            self._validation_errors.add(e)
+            self._columns_ok = False
+
+    def _parse_line(self, line):
+        line = dict(zip(self._columns, line))
+        self._process_line(line)
+
+    def _prep_reader(self, psl):
+        reader = csv.reader(psl, skipinitialspace=True, quotechar='"', delimiter=",")
+        self._check_columns(reader)
+
+        if not self._columns_ok:
+            logger.warn("Expected Columns Not Found in Provided PSL. "
+                        "Not Parsing : " + self._psl_path)
+            return
+        return reader
+
+    def cleanup(self):
         pass
+
+
+class PslParserTMIR(PslParserCSV):
+    _meta = ["CCODE", "VERSION", "COACHNAME"]
+    _owner_ident_name = ["{0} v{1}", "CCODE", "VERSION"]
+    _owner_desc_name = ["{}", "COACHNAME"]
+
+    _expected_columns = ["Page", "PsNo", "Lv", "DrawingNo", "Alt", "Item",
+                         "SDrawingNo", "SAlt", "Description", "St", "QPC"]
+
+    _ident_name = [("{0} {1}", "SDrawingNo", "SAlt"), ("{}", "Description",)]
+    _parent_ident_name = [("{0} {1}", "DrawingNo", "Alt")]
+
+    _level_name = "Lv"
+    _qty_name = "QPC"
+    _refdes_name = "Item"
+    _desc_name = "Description"
+
+    _type_name = "St"
+    _type_assembly = "AS"
+    _type_part = "CO"
+
+    _handle_qty = False
 
 
 if __name__ == '__main__':
     pslpath = "/home/chintal/orgs/scratch/MD685v08 LHB SCN Shell.csv"
-
